0eef614dfbb1bc472a5e22f21383311dad1c193e
Kashif Ali <technicalkashifmehrvi@gmail.com>
2026-09-20 03:52:21 -0400

    branding: replace runtime logo with LBK

--- ui/logo.py
+++ ui/logo.py
@@ -17,14 +17,14 @@ def clear():
 def logo():
     clear()
     print(f"""{GREEN}
-      .d8888.  db    db  d8888b.
-      88'  YP  `8b  d8'  88  `8D
-      `8bo.     `8bd8'   88oobY'
-        `Y8b.   .dPYb.   88`8b
-      db   8D  .8P  Y8.  88 `88.
-      `8888Y'  YP    YP  88   YD   {ORANGE}V-{VERSION}
+      ##        ######   ##  ##
+      ##        ##   ##  ##  ##
+      ##        ######   #####
+      ##        ##   ##  ##  ##
+      #######   ######   ##  ##
+      {ORANGE}LBK Automation  V-{VERSION}
 {LINE}
- {GREEN}[{RED}●{GREEN}] TOOL OWNER   {CYAN}:{GREEN} @mrsxrtool
+ {GREEN}[{RED}●{GREEN}] TOOL OWNER   {CYAN}:{GREEN} LBK Automation
  {GREEN}[{RED}●{GREEN}] TOOL         {CYAN}:{GREEN} AUTO CREATE
  {GREEN}[{RED}●{GREEN}] TOOL STATUS  {CYAN}:{GREEN} PAID
 {LINE}""")
